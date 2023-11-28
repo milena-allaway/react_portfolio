a071e645384bf1436936679b095b768ccbbbe029
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-11-27 23:51:23 -0500

    set up file structure, and code for main.jsx

---
+++ react-portfolio/src/main.jsx
@@ -0,0 +1,52 @@
+// Desc: This is the main file for the react-portfolio app
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
+// import pages that router will use
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
+
+// routes and corresponding components
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />,
+      },
+      {
+        path: '/portfolio',
+        element:  <PortfolioPage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
